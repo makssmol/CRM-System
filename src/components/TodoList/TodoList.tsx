c8d2af1936f465bd5a6f019d3dee2298b1bcce5a
makssmol <gutarteptail@gmail.com>
2026-05-12 10:34:00 +0300

    Типизировал компонент TodoList

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,13 +1,15 @@
+import type React from "react";
 import { TodoItem } from "../TodoItem";
+import type { TasksData } from "../../types/basicTypes";
 
-export function TodoList({
-  loadTasks,
-  setError,
-  isFetching,
-  task,
-  selectedTask,
-}) {
-  console.log("render");
+export const TodoList: React.FC<{
+  loadTasks: (arg: string) => void;
+  setError: (errorMessage: string | null) => void;
+  isFetching: boolean;
+  task: TasksData[];
+  selectedTask: string;
+}> = (props) => {
+  const { loadTasks, setError, isFetching, task, selectedTask } = props;
   return (
     <>
       {isFetching && <p>Tasks are loading</p>}
@@ -25,4 +27,4 @@ export function TodoList({
         ))}
     </>
   );
-}
+};
